e5e7e3aff571d9200274126bb2e2c243d6b198ca
Jared Forsyth <jared@jaredforsyth.com>
2013-06-29 22:24:17 -0600

    test suite ftw; just refactored everything, and all tests passing

--- index.js
+++ index.js
@@ -1,8 +1,186 @@
 
 var valid = {
   sub: /^\w+$/,
-  name: /^\w+(\.\w+)*$/
+  name: /^\w+(\.\w+)*$/,
+  key: /^[^.]$/
 };
+
+var SettingsManager = function (name, options) {
+  if (name.indexOf('.') !== -1) {
+    throw new Error('Invalid manager name; must not contain "."');
+  }
+  this.name = name;
+  this.all = {};
+  this.settings = {};
+}
+
+SettingsManager.prototype = {
+
+  // setup settings.
+  // items: { key: setting, ... }
+  //   key: can't contain "."
+  //   setting: [ single | group ]
+  //   group: { _group: true, key: setting, [key:setting, ...]}
+  //   single: {
+  //     title: str,
+  //     description: str,
+  //     class: HTML class to be applied when displayed
+  //     type: str
+  //     ... type-specific options
+  //   }
+  //
+  // prefix: string; the place to insert the settings. "." separated path
+  // override: (default false). If false, an error is raised if a
+  //           setting is already present.
+  config: function (items, prefix, override) {
+    var base = this.all;
+    if (prefix) {
+      prefix.split('.').forEach(function (item) {
+        if (!base[item]) base[item] = {};
+        base = base[item];
+      });
+    }
+    if (!items._type) items._type = 'bool';
+    items._group = true;
+    this.add(items, base, prefix, override);
+  },
+
+  // get a setting value. name is a "." separated path
+  get: function (name) {
+    if (!this.settings[name]) {
+      console.warn('Unknown setting requested:', name);
+      return;
+    }
+    return this.settings[name].value;
+  },
+
+  set: function (name, value) {
+    if (!this.settings[name]) {
+      console.warn('Trying to set an unknown setting:', name);
+      return;
+    }
+    // TODO: some kind of validation here?
+    // I figure if you're setting it manually, ou know what you're doing.
+    this.settings[name].value = value;
+  },
+
+  // create a proxy, which treats all commands as relative to the provided path
+  proxy: function (path) {
+    var self = this;
+    return {
+      parent: this,
+      config: function (items, prefix, override) {
+        prefix = prefix ? path + '.' + prefix : path;
+        return self.config(items, prefix, override);
+      },
+      get: function (key) {
+        return self.get(path + '.' + key);
+      },
+      set: function (key, value) {
+        return self.set(path + '.' + key, value);
+      },
+      getList: function (keys) {
+        return self.getList(keys, path);
+      },
+      getHash: function (hash) {
+        return self.getHash(hash, path);
+      },
+      getHaskKeys: function (hash) {
+        return self.getHashKeys(hash, path);
+      }
+    };
+  },
+
+  // TODO: make this namespaceable? Or glob-style?
+  json: function () {
+    var result = {};
+    for (var key in this.settings) {
+      result[key] = this.settings[key].value;
+    }
+    return result;
+  },
+  
+  getList: function (list, path) {
+    var self = this;
+    path = path ? path + '.' : '';
+    return list.map(function (item) {
+      return self.get(path + item);
+    });
+  },
+
+  getHash: function (hash, path) {
+    var obj = {};
+    path = path ? path + '.' : '';
+    for (var key in hash) {
+      obj[key] = this.get(path + hash[key]);
+    }
+    return obj;
+  },
+
+  getHashKeys: function (hash, path) {
+    var obj = {};
+    path = path ? path + '.' : '';
+    for (var key in hash) {
+      obj[this.get(path + key)] = hash[key];
+    }
+    return obj;
+  },
+
+  // Used internally to recursively add setting definitions
+  // items; see config
+  // base: the object to which settings will be added. Should corrospond to the "path"
+  // path: "." separated path to the given base
+  // override: see config
+  add: function (items, base, path, override) {
+    if (!items._group) {
+      throw new Error('you can only add groups');
+    }
+    path = path ? path + '.' : '';
+    for (var key in items) {
+      if (key === '_group' || key === '_type' || key === '_name') continue;
+      if (key[0] === '_' || key.indexOf('.') !== -1) {
+        throw new Error('invalid key name: ' + key + '. No periods and can\'t start with an underscore');
+      }
+      if (items[key]._group) {
+        if (!base[key]) base[key] = {};
+        items[key]._name = key;
+        if (!items[key]._type) {
+          items[key]._type = items._type;
+        }
+        this.add(items[key], base[key], path + key, override);
+        continue;
+      }
+      if (base[key] && !override) {
+        throw new Error('duplicate delaration of ' + key);
+      }
+      base[key] = items[key];
+      items[key].name = key;
+      if (!items[key].type) {
+        items[key].type = items._type;
+      }
+      this.settings[path + key] = items[key]
+    }
+  },
+};
+
+var single = new SettingsManager('default')
+  , managers = {default: single};
+
+module.exports = function getSub(name) {
+  if (!name) return single;
+  return single.proxy(name);
+};
+
+module.exports.SettingsManager = SettingsManager;
+module.exports.getSettings = function(name) {
+  name = name || 'default';
+  if (!managers[name]) {
+    managers[name] = new SettingsManager(name);
+  }
+  return managers[name];
+};
+  
+/*
 
 var SettingsManager = function (name, options) {
   this.name = name;
@@ -141,7 +319,6 @@ SettingsManager.prototype = {
     }
   },
 
-/** get a list of settings values **/
   getList: function (data, sub) {
     var res = [];
     sub = sub || '';
@@ -151,7 +328,6 @@ SettingsManager.prototype = {
     return res;
   },
 
-/** { key: settingName, ... } -> { key: settingValue } **/
   getHash: function (data, sub) {
     var res = {}
       , keys = Object.keys(data);
@@ -162,7 +338,6 @@ SettingsManager.prototype = {
     return res;
   },
 
-/** { settingName: value, ... } -> { settingValue: value, ... } */
   getHashKeys: function (data, sub) {
     var res = {}
       , keys = Object.keys(data);
@@ -187,4 +362,4 @@ single.getSettings = function(name) {
   }
   return managers[name];
 };
-
+*/
--- test/tests.js
+++ test/tests.js
@@ -8,15 +8,9 @@ describe('SettingsManager, when initialized', function(){
     mgr = new settings.SettingsManager("test");
   });
 
-  describe('sub', function () {
-    it('should fail on bad sub name', function () {
-      expect(mgr.sub.bind(mgr, 'a ')).to.throw(/alphanumeric/);
-    });
-  });
-
   describe('with a single item', function(){
     beforeEach(function(){
-      mgr.add({name: 'key', value: true});
+      mgr.config({key: {value: true}});
     });
 
     it('should retrieve', function(){
@@ -27,20 +21,17 @@ describe('SettingsManager, when initialized', function(){
       mgr.set('key', false);
       expect(mgr.get('key')).to.be.false;
     });
-
-    it('should default to type bool', function(){
-      expect(mgr.getType('key')).to.eql('bool');
-    });
   });
 
   describe('with a nested item', function(){
     beforeEach(function(){
-      mgr.add({name: 'top',
-               type: 'text',
-               settings: [
-                 {name: 'sub', type: 'bool', value: true},
-                 {name: 'sub2', value: 'yes'}
-               ]});
+      mgr.config({
+        top: {
+          _group: true,
+          _type: 'text',
+          sub: {type: 'bool', value: true},
+          sub2: {value: 'yes'}
+        }});
     });
 
     it('should be accessible as a.b', function(){
@@ -49,11 +40,11 @@ describe('SettingsManager, when initialized', function(){
     });
 
     it('should inherit type when not specified', function(){
-      expect(mgr.getType('top.sub2')).to.eql('text');
+      expect(mgr.settings['top.sub2'].type).to.eql('text');
     });
 
     it('should not inherit type when specified', function(){
-      expect(mgr.getType('top.sub')).to.eql('bool');
+      expect(mgr.settings['top.sub'].type).to.eql('bool');
     });
 
     it('should only include settings in the json', function(){
@@ -64,8 +55,8 @@ describe('SettingsManager, when initialized', function(){
 
   describe('with multiple items', function(){
     beforeEach(function(){
-      mgr.add({name: 'xav', value: false});
-      mgr.add({name: 'abc', value: true});
+      mgr.config({xav: {value: false}});
+      mgr.config({abc: {value: true}});
     });
 
     it('should serialize', function(){
@@ -77,13 +68,13 @@ describe('SettingsManager, when initialized', function(){
   describe('with a sub', function () {
     var sub;
     beforeEach(function () {
-      sub = mgr.sub('test');
-      sub.add({name: 'one', value: true});
-      sub.add({name: 'two', value: 'man'});
+      sub = mgr.proxy('test');
+      sub.config({one: {value: true}});
+      sub.config({two: {value: 'man'}});
     });
 
     it('should add things namespaced', function () {
-      expect(mgr.get('test:one')).to.be.true;
+      expect(mgr.get('test.one')).to.be.true;
     });
 
     it('should getList namespaced', function () {
